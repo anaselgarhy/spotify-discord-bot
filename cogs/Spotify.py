2f83b94248f1024716cf77eacdad2573699ee797
shahd <0x73hahd@gmail.com>
2022-12-27 17:10:36 +0200

    Add recommendations command

--- cogs/Spotify.py
+++ cogs/Spotify.py
@@ -11,10 +11,12 @@ cwd = str(cwd)
 hiding = json.load(open(cwd + '/hiding.json'))
 client_id = hiding['client_id']
 client_secret = hiding['client_secret']
+redirect_uri = hiding['redirect_uri']
 token_spotify = tk.request_client_token(client_id=client_id,
                                         client_secret=client_secret)
 spotify = tk.Spotify(token_spotify,
                      asynchronous=True)
+scope = tk.scope.user_top_read + tk.scope.playlist_modify_private
 
 
 class Spotify(commands.Cog):
@@ -37,6 +39,34 @@ class Spotify(commands.Cog):
         embed.set_thumbnail(url="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Green.png")
         embed.set_footer(text="Requested by " + ctx.author.display_name)
         for t in tracks.items:
+            artist = t.artists[0].name
+            url = t.external_urls["spotify"]
+
+            message = "\n".join([
+                f"[Open on Spotify](" + url + ")",
+                ":musical_note: " + artist,
+                ":star: " + t.album.name
+            ])
+            embed.add_field(name=t.name, value=message, inline=False)
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=['re'])
+    async def recommend(self, ctx):
+        spotify_token = tk.prompt_for_user_token(client_id=client_id,
+                                                 client_secret=client_secret,
+                                                 redirect_uri=redirect_uri,
+                                                 scope=scope)
+        spotify2 = tk.Spotify(spotify_token)
+
+        top_tracks = spotify2.current_user_top_tracks(limit=5).items
+        top_track_ids = [t.id for t in top_tracks]
+        recommendations = spotify2.recommendations(track_ids=top_track_ids, limit=5).tracks
+
+        embed = Embed(title="Recommendations", color=0x921030)
+        embed.set_thumbnail(url="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Green.png")
+        embed.set_footer(text="Requested by " + ctx.author.display_name)
+
+        for t in recommendations:
             artist = t.artists[0].name
             url = t.external_urls["spotify"]
 
